66fe8a1642d4e61374e9a45261f22cf5d3801e43
Pecacheu <3608878+Pecacheu@users.noreply.github.com>
2026-06-25 23:54:15 -0400

    fix: Build static file for Docker mode - Add CORS header
    Signed-off-by: Pecacheu <3608878+Pecacheu@users.noreply.github.com>

--- packages/client/app-config.plugin.ts
+++ packages/client/app-config.plugin.ts
@@ -1,4 +1,4 @@
-import { ViteDevServer } from "vite";
+import { PluginOption } from "vite";
 
 //Load .env file
 import dotenv from "dotenv";
@@ -7,18 +7,32 @@ dotenv.config({ quiet: true });
 import CONFIGURATION, { AppConfig } from "./components/common/lib/env";
 
 //Data for public config endpoint
-const appCfg: AppConfig = {
+const appCfg = JSON.stringify({
   api: CONFIGURATION.DEFAULT_API_URL,
   gifbox: CONFIGURATION.DEFAULT_GIFBOX_URL,
-};
+} as AppConfig);
 
-export default function appConfigPlugin() {
+export default function appConfigPlugin(): PluginOption {
   return {
     name: "app-config",
-    configureServer(server: ViteDevServer) {
-      server.middlewares.use("/.stoat-config", (_, res) => {
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(appCfg));
+    //For dev / serve mode
+    configureServer(server) {
+      server.middlewares.use("/stoat-config.json", (_, res) => {
+        res.writeHead(200, {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        });
+        res.end(appCfg);
+      });
+    },
+    //For static build
+    buildStart() {
+      //TODO: This throws warning "context method emitFile() is not supported in serve mode. This plugin is likely not vite-compatible."
+      //Need to add a conditional and detect serve mode somehow
+      this.emitFile({
+        fileName: "stoat-config.json",
+        source: appCfg,
+        type: "asset",
       });
     },
   };
